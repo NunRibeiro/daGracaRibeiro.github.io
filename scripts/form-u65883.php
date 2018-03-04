6f4469d693df01f9a8c461a8f1e55d4a62341fb7
NunRibeiro <rafadub_13@hotmail.com>
2018-03-04 22:04:05 +0000

    03 2018

--- scripts/form-u65883.php
+++ scripts/form-u65883.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'n.gracaribeiro.arq@outlook.com',
-		'to' => 'n.gracaribeiro.arq@outlook.com'
+		'from' => 'nuno@dagracaribeiro.pt',
+		'to' => 'nuno@dagracaribeiro.pt'
 	),
 	'fields' => array(
 		'custom_U65891' => array(
